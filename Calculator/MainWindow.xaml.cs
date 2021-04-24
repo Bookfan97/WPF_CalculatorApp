4cff7ba71e5da61ee6da565d3a69d2222fa7e745
Noah Day <sheltielover432@gmail.com>
2021-04-23 23:02:33 -0400

    Button Setup

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,139 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+        SelectedOperators selectedOperator;
+
         public MainWindow()
         {
             InitializeComponent();
+            //resultLabel.Content = "8675309";
+            
+            //Function Buttons
+            ACButton.Click += ACButton_Click;
+            signChangeButton.Click += SignChangeButton_Click;
+        }
+
+        private void SignChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            double resultNum;
+            if(double.TryParse(resultLabel.Content.ToString(), out resultNum))
+            {
+                switch(selectedOperator)
+                { 
+                    case SelectedOperators.Addition:
+                        result = SimpleMath.Add(lastNumber, resultNum);
+                    break;
+                    case SelectedOperators.Subtraction:
+                        result = SimpleMath.Subtract(lastNumber, resultNum);
+                    break;
+                    case SelectedOperators.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, resultNum);
+                    break;
+                    case SelectedOperators.Division:
+                        result = SimpleMath.Divide(lastNumber, resultNum);
+                    break;
+                }
+                resultLabel.Content = result.ToString();
+            }
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void ACButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content="0";
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Previous value saved as out variable
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+            }
+            if(sender == multiplyButton)
+            {
+                selectedOperator = SelectedOperators.Multiplication;
+            }
+            if(sender == divideButton)
+            {
+                selectedOperator = SelectedOperators.Division;
+            }
+            if(sender == plusButton)
+            {
+                selectedOperator = SelectedOperators.Addition;
+            }
+            if(sender == minusButton)
+            {
+                selectedOperator = SelectedOperators.Subtraction;
+            }
+        }
+
+        private void periodButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!resultLabel.Content.ToString().Contains("."))
+            { 
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+        }
+
+        private void numberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue =  int.Parse((sender as Button).Content.ToString());
+            
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+    }
+
+    public enum SelectedOperators
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add( double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtract( double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiply( double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Divide( double n1, double n2)
+        {
+            return n1 / n2;
         }
     }
 }
